de01bb0d35f0a3d6a31c434ded2d19e0c2f9ca70
Jonas Rapp <jonas.rapp@cinteros.se>
2017-01-25 16:49:54 +0100

    Added sample plugin AccountVerification

---
+++ SamplePlugin/AccountVerification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonasPluginBase;
+using Microsoft.Xrm.Sdk;
+
+namespace SamplePlugin
+{
+    public class AccountVerification : JonasPluginBase.JonasPluginBase
+    {
+        public override void Execute(JonasPluginBag bag)
+        {
+            VerifyAccountInfo(bag);
+        }
+
+        private static void VerifyAccountInfo(JonasPluginBag bag)
+        {
+            if (bag.TargetEntity.Contains("fax"))
+            {
+                throw new InvalidPluginExecutionException("We don't allow use of Fax attribute");
+            }
+            else
+            {
+                bag.Trace("All is well.");
+            }
+        }
+    }
+}
