ddbef5285bb551a1d7d1dd85442e4bb071596fea
Jonas Rapp <jonas.rapp@cinteros.se>
2017-02-13 08:41:31 +0100

    Adding nuspec file

--- JonasPluginBase/Properties/AssemblyInfo.cs
+++ JonasPluginBase/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JonasPluginBase")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright Jonas Rapp 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
