1146c9797d0fa2aeeca857f1a495fe04147a3660
Jonas Rapp <jonas.rapp@cinteros.se>
2017-02-27 15:41:31 +0100

    Added ContextLogger sample plugin

---
+++ SamplePlugin/ContextLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonasPluginBase;
+
+namespace SamplePlugin
+{
+    public class ContextLogger : JonasPluginBase.JonasPluginBase
+    {
+        public override void Execute(JonasPluginBag bag) { }
+    }
+}
